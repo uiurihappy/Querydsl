bd57331d131f34b39a99dc80e8d418af8c758107
ybchar <uiurihappy@sju.ac.kr>
2023-02-14 18:22:38 +0900

    feat: querydsl configurationing

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,40 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+//	@Autowired
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+
+		QHello qHello = new QHello("h");
+
+		// query 와 관련된 거는 q타입을 넣는다.
+		Hello hello1 = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertEquals(hello1, hello);
+
 	}
 
 }
