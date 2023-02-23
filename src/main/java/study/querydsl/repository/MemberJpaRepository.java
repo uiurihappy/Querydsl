2ee653eb362fdb9c157cae7de99ab232a977fdb8
ybchar <uiurihappy@sju.ac.kr>
2023-02-23 15:47:40 +0900

    feat: Member Jpa repo

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
 
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 class MemberJpaRepository {
@@ -17,12 +17,29 @@ class MemberJpaRepository {
 		this.queryFactory = new JPAQueryFactory(em);
 	}
 
+	// Member save
 	public void save(Member member) {
 		em.persist(member);
 	}
 
+	// select Member By id
 	public Optional<Member> findById(Long id) {
 		Member findMember = em.find(Member.class, id);
 		return Optional.ofNullable(findMember);
 	}
+
+	// select Member list
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+
+	public List<Member> findByUsername(String username) {
+		return em.createQuery(
+				"select m from Member m " +
+						"where m.username = :username", Member.class)
+				.setParameter("username", username)
+				.getResultList();
+	}
+
 }
